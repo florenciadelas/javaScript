dbc7f4440151637519aa01cb203d09f14ef3c88f
florenciadelas <florenciadelas@hotmail.com>
2022-05-05 16:43:28 -0300

    primer desafio

---
+++ Js/proyecto.js
@@ -0,0 +1,12 @@
+// Ejercicio para agregar ingredientes al menu
+
+let ingrediente = prompt ("Ingrese el ingrediente que quiere en su menu: 1. Arroz 2. Tomate 3. Lechuga 4. FIN")
+
+let confirmaMenu = "FIN";
+
+while (ingrediente !== confirmaMenu){
+  
+    ingreditente = prompt ("Ingrese el ingrediente que quiere en su menu: 1. Arroz 2. Tomate 3. Lechuga 4. FIN")
+}
+
+
