00dfb644cbf4e823616f483b8d8cf7022fdab408
florenciadelas <florenciadelas@hotmail.com>
2022-05-24 15:30:28 -0300

    primera pre entrega

--- Js/funciones.js
+++ Js/funciones.js
@@ -6,25 +6,27 @@ let vestido = 500
 let resultado = 0
 let carrito = []
 let valorCarrito = 0
+let tipoProducto = []
+
 
 // Objeto cliente
 
-class Cliente{
+class Cliente {
 
-    constructor(nombre, apellido, id){
+    constructor(nombre, apellido) {
         this.nombre = nombre
         this.apellido = apellido
     }
 
-    nombreCompleto(){
-        return this.nombre+ " " + this.apellido
+    nombreCompleto() {
+        return this.nombre + " " + this.apellido
     }
 }
 
 let nombre = prompt("Ingrese su nombre")
 let apellido = prompt("Ingrese su apellido")
 
-const cliente = new Cliente (nombre, apellido)
+const cliente = new Cliente(nombre, apellido)
 
 
 // Array para agregar productos
@@ -38,11 +40,11 @@ while (producto !== "FIN") {
         producto = prompt("Error! Ingrese el producto que desea comprar: REMERA, PANTALON, VESTIDO o FIN")
     }
 
-    let cantidad = prompt("Ingrese la cantidad que desea comprar.")
+    let cantidad = Number(prompt("Ingrese la cantidad que desea comprar."))
 
-    devolverResultado = (a, b) =>{
+    devolverResultado = (a, b) => {
         return resultado = a * b
-    } 
+    }
 
     mostrarValor = () => {
         if (producto == "REMERA") {
@@ -55,27 +57,84 @@ while (producto !== "FIN") {
         }
     }
 
-  
     mostrarValor()
 
     carrito.push(resultado)
 
+    tipoProducto.push(producto)
+
+    eliminarProducto = () => {
+        productoEliminado = prompt("Desea eliminar el producto? SI/NO")
+
+        while (productoEliminado !== "SI" && productoEliminado !== "NO") {
+            productoEliminado = prompt("Error! Ingrese una respuesta valida. SI/NO")
+        }
+
+        if (productoEliminado === "SI")
+            carrito.pop()
+    }
+
+    eliminarProducto()
+
+    if (productoEliminado === "SI") {
+        tipoProducto.pop()
+    }
+
     producto = prompt("Ingrese el producto que desea comprar: REMERA, PANTALON, VESTIDO, o FIN para finalizar la compra")
 }
 
-for (i=0; i<carrito.length; i++){
+for (i = 0; i < carrito.length; i++) {
     valorCarrito += carrito[i]
 }
 
+
 console.log("Nombre del cliente")
 console.log(cliente)
+console.log("Tipo de productos")
+console.log(tipoProducto)
 console.log("Lista de compras")
 console.log(carrito)
+console.log("Cantidad de productos a comprar")
+console.log(carrito.length)
 console.log("Valor final")
-console.log(valorCarrito)
+console.log("$" + valorCarrito)
 
+let consultaEliminar = prompt("Desea eliminar algun producto? SI/NO")
 
+while (consultaEliminar === "SI") {
+    if (consultaEliminar === "SI") {
+        let prodAEliminar = (Number(prompt("Indique el numero de producto que desea eliminar de la lista")))
 
+        eliminarProdDeLista = () => {
+            let prodEliminadoDeCarrito = carrito.splice(prodAEliminar - 1, 1) // AGREGUÉ EL -1 PARA QUE EL USUARIO ELIJA A PARTIR DEL PRODUCTO 1 Y NO DEL 0
+            let prodEliminadoDeProducto = tipoProducto.splice(prodAEliminar - 1, 1)
+            valorCarrito -= prodEliminadoDeCarrito
+        }
+    } else {
+        alert("Gracias por su compra!")
+    }
+    eliminarProdDeLista()
 
+    console.log("Lista de compras actualizada")
+    console.log(carrito)
+    console.log("Tipo de productos actualizada")
+    console.log(tipoProducto)
+    console.log("Cantidad de productos a comprar")
+    console.log(carrito.length)
+    console.log("Valor actualizado")
+    console.log("$" + valorCarrito)
+    consultaEliminar = prompt("Desea eliminar algun otro producto? SI/NO")
+
+}
+
+let prodConIva = prompt("Desea ver el valor de los productos con IVA?")
+
+if (prodConIva === "SI") {
+    carrito.forEach((prod) => {
+        console.log("El valor del producto con IVA es $" + (prod *= 1.21))
+    })
+}else {
+    alert("Gracias por su compra!")
+}
 
 
