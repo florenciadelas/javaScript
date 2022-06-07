8968751cd579fca19718788f6091830fb9ab8f93
florenciadelas <florenciadelas@hotmail.com>
2022-06-07 18:44:08 -0300

    Segunda entrega

--- Js/funciones.js
+++ Js/funciones.js
@@ -7,7 +7,8 @@ let resultado = 0
 let carrito = []
 let valorCarrito = 0
 let tipoProducto = []
-precioUnitario = 0
+let suma = []
+
 
 
 // Objeto cliente
@@ -24,19 +25,35 @@ class Cliente {
     }
 }
 
+class Carrito {
+
+    constructor(producto, cant) {
+        this.producto = producto
+        this.cant = cant
+    }
+}
+
 const btnAgregar = document.getElementById("agregarAlCarrito")
 btnAgregar.addEventListener("click", () => {
 
-let nombre = document.getElementById("nombre")
-let apellido = document.getElementById("apellido")
-const cliente = new Cliente(nombre, apellido)
-
-let titluloPagina = document.getElementById("titulo")
-titluloPagina.innerHTML = `Bienvenid@ ${nombre.value} ${apellido.value}`
+    let nombre = document.getElementById("nombre")
+    localStorage.setItem("NOMBRE-USUARIO", nombre.value)
 
 
-let producto = document.getElementById("producto")
-let cant = document.getElementById("cantidad")
+    let apellido = document.getElementById("apellido")
+    localStorage.setItem("APELLIDO-USUARIO", apellido.value)
+
+    const cliente = new Cliente(nombre, apellido)
+
+    let titluloPagina = document.getElementById("titulo")
+    titluloPagina.innerHTML = `Bienvenid@ ${nombre.value} ${apellido.value}`
+
+
+    let producto = document.getElementById("producto")
+    localStorage.setItem("PRODUCTO", producto.value)
+
+    let cant = document.getElementById("cantidad")
+    localStorage.setItem("CANTIDAD", cant.value)
 
 
     devolverResultado = (a, b) => {
@@ -55,65 +72,27 @@ let cant = document.getElementById("cantidad")
 
     mostrarValor()
 
-    carrito.push(resultado)
-    tipoProducto.push(producto)
-
-    // eliminarProducto = () => {
-    //     productoEliminado = prompt("Desea eliminar el producto? SI/NO")
-
-    //     while (productoEliminado !== "SI" && productoEliminado !== "NO") {
-    //         productoEliminado = prompt("Error! Ingrese una respuesta valida. SI/NO")
-    //     }
-
-    //     if (productoEliminado === "SI")
-    //         carrito.pop()
-    // }
-
-    // eliminarProducto()
-
-    //     if (productoEliminado === "SI") {
-    //         tipoProducto.pop()
-    //     }
-
-    //     producto = prompt("Ingrese el producto que desea comprar: REMERA, PANTALON, VESTIDO, o FIN para finalizar la compra")
-    // }
-
-    // let consultaEliminar = prompt("Desea eliminar algun producto? SI/NO")
-
-    // while (consultaEliminar === "SI") {
-    //     if (consultaEliminar === "SI") {
-    //         let prodAEliminar = (Number(prompt("Indique el numero de producto que desea eliminar de la lista")))
-
-    //         eliminarProdDeLista = () => {
-    //             let prodEliminadoDeCarrito = carrito.splice(prodAEliminar - 1, 1) // AGREGUÉ EL -1 PARA QUE EL USUARIO ELIJA A PARTIR DEL PRODUCTO 1 Y NO DEL 0
-    //             let prodEliminadoDeProducto = tipoProducto.splice(prodAEliminar - 1, 1)
-    //             valorCarrito -= prodEliminadoDeCarrito
-    //         }
-    //     } else {
-    //         alert("Gracias por su compra!")
-    //     }
-    //     eliminarProdDeLista()
-
-
-    // let prodConIva = prompt("Desea ver el valor de los productos con IVA?")
-
-    // if (prodConIva === "SI") {
-    //     carrito.forEach((prod) => {
-    //         console.log("El valor del producto con IVA es $" + (prod *= 1.21))
-    //     })
-    // } else {
-    //     alert("Gracias por su compra!")
-    // }
 
     function mostrarCompra() {
+        if (producto.value !== "0" && cant.value) {
 
-        let nombreProducto = document.getElementById("contenedor")
-        const ul = document.createElement("ul")
-        ul.innerHTML = `Producto ` + producto.value + " $" + resultado
-        contenedor.appendChild(ul)
+            carrito.push(resultado)
+            tipoProducto.push(producto)
+
+            let nombreProducto = document.getElementById("contenedor")
+            const ul = document.createElement("ul")
+            ul.innerHTML = `Producto ` + producto.value + " $" + resultado
+            contenedor.appendChild(ul)
+        }
     }
-
     mostrarCompra()
+
+    let carro = new Carrito(producto.value, cant.value)
+    suma.push(carro)
+    localStorage.setItem("carritos", JSON.stringify(suma))
+
+    producto.value = "0"
+    cant.value = ""
 
 })
 
@@ -121,6 +100,11 @@ let cant = document.getElementById("cantidad")
 const btnFinalizar = document.getElementById("finalizar")
 btnFinalizar.addEventListener("click", () => {
 
+    if (carrito.length === 0) {
+        return
+    }
+
+    btnAgregar.setAttribute("style", "display:none")
 
     for (i = 0; i < carrito.length; i++) {
         valorCarrito += carrito[i]
@@ -137,6 +121,9 @@ btnFinalizar.addEventListener("click", () => {
         tituloH4.innerText = ("Gracias por su compra!")
         contenedor.appendChild(tituloH4)
     }
+    carrito = []
     mostrarValorTotal()
     agradecer()
 })
+
+
